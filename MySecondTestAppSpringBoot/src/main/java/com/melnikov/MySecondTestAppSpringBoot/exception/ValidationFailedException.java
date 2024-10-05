5073c6c40e9409d285e91da7dba0d53b0ffa450a
YuriMelnikov <melnikov_203@mail.ru>
2024-10-05 11:58:02 +0500

    Init commit

---
+++ MySecondTestAppSpringBoot/src/main/java/com/melnikov/MySecondTestAppSpringBoot/exception/ValidationFailedException.java
@@ -0,0 +1,5 @@
+package com.melnikov.MySecondTestAppSpringBoot.exception;
+
+public class ValidationFailedException extends Exception {
+    public ValidationFailedException(String message) {super(message);}
+}
---
+++ MySecondTestAppSpringBoot/src/test/java/com/melnikov/MySecondTestAppSpringBoot/MySecondTestAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melnikov.MySecondTestAppSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecondTestAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
